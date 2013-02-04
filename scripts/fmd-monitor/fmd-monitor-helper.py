ac136f2ce24e75aee6c57b91329483c420d0585b
Johnny Huang <hzqtc1229@gmail.com>
2013-02-04 22:13:28 +0800

    fmd monitor: detects fmd connection error

--- scripts/fmd-monitor/fmd-monitor-helper.py
+++ scripts/fmd-monitor/fmd-monitor-helper.py
@@ -8,30 +8,43 @@ import socket
 def timestamp(seconds):
     return "%d:%02d" % (seconds / 60, seconds % 60)
 
-fmdSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-fmdSock.connect(("localhost", 10098))
-fmdSock.send("info")
-response = fmdSock.recv(4096)
-jsonObj = json.loads(response)
-
 scriptDir = os.path.dirname(os.path.abspath(__file__))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.connect(("localhost", 10098))
+except socket.error:
+    s.close()
+    s = None
 
-status = jsonObj["status"]
-icon = os.path.join(scriptDir, "%s.png" % status)
-if status == "stop":
-    text = "Not Playing"
+if s:
+    s.send("info")
+    response = s.recv(4096)
+    s.close()
+    jsonObj = json.loads(response)
+
+    status = jsonObj["status"]
+    if status == "stop":
+        text = "Not Playing"
+        info1 = ""
+        info2 = ""
+        info3 = ""
+    else:
+        text = "%s / %s" % (timestamp(jsonObj["pos"]), timestamp(jsonObj["len"]))
+        info1 = "%s - %s" % (jsonObj["artist"], jsonObj["title"])
+        info2 = "%s (%d)" % (jsonObj["album"], jsonObj["year"])
+        info3 = "-"
+else:
+    status = "error"
+    text = "Contact Fmd Failed"
     info1 = ""
     info2 = ""
     info3 = ""
-else:
-    text = "%s / %s" % (timestamp(jsonObj["pos"]), timestamp(jsonObj["len"]))
-    info1 = "%s - %s" % (jsonObj["artist"], jsonObj["title"])
-    info2 = "%s (%d)" % (jsonObj["album"], jsonObj["year"])
-    info3 = "-"
 
 env = Environment(loader = FileSystemLoader(scriptDir))
 template = env.get_template("template.json")
-print template.render(icon = icon,
+print template.render(
+        status = status,
+        icon = os.path.join(scriptDir, "%s.png" % status),
         text = text,
         info1 = info1,
         info2 = info2,
