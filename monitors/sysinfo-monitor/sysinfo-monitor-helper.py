0a5aa7bfccfa1bb2f4d8d5b5049bdc01c321e471
Johnny Huang <hzqtc1229@gmail.com>
2013-02-06 12:21:20 +0800

    sysinfo-monitor: improve figure accuracy

--- monitors/sysinfo-monitor/sysinfo-monitor-helper.py
+++ monitors/sysinfo-monitor/sysinfo-monitor-helper.py
@@ -13,11 +13,18 @@ text = "%.1lf%%" % overall_cpu_percent
 cpu_ids = range(len(cpu_percent_list))
 cpu = "; ".join(map(lambda x: "Core %d: %.1lf%%" % (x[0], x[1]), zip(cpu_ids, cpu_percent_list)))
 
-memory = "Total: %dM; Available: %dM; Used: %dM; Free: %dM" % (
-    memory_usage.total / (1 << 20),
-    memory_usage.available / (1 << 20),
-    memory_usage.used / (1 << 20),
-    memory_usage.free / (1 << 20))
+# only the following figures are correct in psutil.virtual_memory()
+mtotal = memory_usage.total / (1 << 20)
+mfree = memory_usage.free / (1 << 20)
+mactive = memory_usage.active / (1 << 20)
+minactive = memory_usage.inactive / (1 << 20)
+# other figures are calculated
+mavailable = mfree + minactive
+mused = mtotal - mfree - minactive
+mwired = mused - mactive
+
+memory1 = "Total: %dM; Available: %dM; Used: %dM" % (mtotal, mavailable, mused)
+memory2 = "Wired: %dM; Active: %dM; Inactive: %dM; Free: %dM" % (mwired, mactive, minactive, mfree)
 
 scriptDir = os.path.dirname(os.path.abspath(__file__))
 env = Environment(loader = FileSystemLoader(scriptDir))
@@ -27,4 +34,5 @@ print template.render(
         altimage = os.path.join(scriptDir, "icon_neg.png"),
         text = text,
         cpu = cpu,
-        memory = memory).encode("utf-8")
+        memory1 = memory1,
+        memory2 = memory2).encode("utf-8")
