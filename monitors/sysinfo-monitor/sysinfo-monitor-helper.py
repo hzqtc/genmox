2d93072e16e81c13fa44cef3d5de44e436706107
Johnny Huang <hzqtc1229@gmail.com>
2013-02-06 10:58:56 +0800

    sysinfo monitor: add image

--- monitors/sysinfo-monitor/sysinfo-monitor-helper.py
+++ monitors/sysinfo-monitor/sysinfo-monitor-helper.py
@@ -8,8 +8,7 @@ cpu_percent_list = psutil.cpu_percent(interval = 1, percpu = True)
 memory_usage = psutil.virtual_memory()
 
 overall_cpu_percent = reduce(lambda x, y: x + y, cpu_percent_list) / len(cpu_percent_list)
-overall_memory_percent = memory_usage.percent
-text = "CPU: %.1lf%% MEM: %.1lf%%" % (overall_cpu_percent, overall_memory_percent)
+text = "%.1lf%%" % overall_cpu_percent
 
 cpu_ids = range(len(cpu_percent_list))
 cpu = "; ".join(map(lambda x: "Core %d: %.1lf%%" % (x[0], x[1]), zip(cpu_ids, cpu_percent_list)))
@@ -24,6 +23,8 @@ scriptDir = os.path.dirname(os.path.abspath(__file__))
 env = Environment(loader = FileSystemLoader(scriptDir))
 template = env.get_template("template.json")
 print template.render(
+        image = os.path.join(scriptDir, "icon.png"),
+        altimage = os.path.join(scriptDir, "icon_neg.png"),
         text = text,
         cpu = cpu,
         memory = memory).encode("utf-8")
