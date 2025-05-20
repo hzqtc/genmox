ad3c4f63308248048bb9093f97aed218af1bb408
Johnny Huang <hzqtc1229@gmail.com>
2025-05-19 20:18:37 -0700

    Get brew pacakge info using parallel.

--- monitors/brew-status/brew-status.sh
+++ monitors/brew-status/brew-status.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 brew="/opt/homebrew/bin/brew"
+parallel="/opt/homebrew/bin/parallel"
 notifier="/opt/homebrew/bin/terminal-notifier"
 
 if test ! -x "$brew"; then
@@ -12,6 +13,16 @@ if test ! -x "$brew"; then
   exit 1
 fi
 
+if test ! -x "$parallel"; then
+  echo '
+  {
+    "imagesymbol": "exclamationmark.triangle",
+    "text": "Error parallel not found"
+  }'
+  exit 1
+fi
+
+# Update brew and check for outdated packages
 $brew update > /dev/null 2>&1
 pkgs=$($brew outdated --verbose)
 
@@ -22,45 +33,52 @@ if [[ -z "$pkgs" ]]; then
     "text": "",
     "menus": [{"text": "Up to date"}]
   }'
-else
-  menuitems='
-  {
-    "click": "'$brew' upgrade",
-    "text": "Upgrade all",
-    "refresh": true
-  }, {
-    "text": "-"
+  exit 0
+fi
+
+get_pkg_menuitem() {
+  # pkg is a string in "name (old_version) < new_version"
+  local pkg="$1"
+  local name=${pkg% (*}
+  local new_version=${pkg#*< }
+  local brew_info=$($brew info --json $name)
+  local desc=$(echo $brew_info | jq -r '.[0] | .desc')
+  local url=$(echo $brew_info | jq -r '.[0] | .homepage')
+  echo '{
+    "text": "'$name'",
+    "subtext": "'$desc'",
+    "badge": "'$new_version'",
+    "click": "/usr/bin/open '$url'"
   },'
-  while IFS= read -r pkg; do
-    # pkg is a string in "name (old_version) < new_version"
-    name=${pkg% (*}
-    new_version=${pkg#*< }
-    brew_info=$($brew info --json $name)
-    desc=$(echo $brew_info | jq -r '.[0] | .desc')
-    url=$(echo $brew_info | jq -r '.[0] | .homepage')
-    menuitems+='
-    {
-      "text": "'$name'",
-      "subtext": "'$desc'",
-      "badge": "'$new_version'",
-      "click": "/usr/bin/open '$url'"
-    },'
-  done <<< "$pkgs"
+}
 
-  echo '
-  {
-    "imagesymbol": "circle.fill",
-    "text": "",
-    "menus": ['$menuitems']
-  }'
+# Export for visibility to parallel
+export brew
+export -f get_pkg_menuitem
 
-  if test -x "$notifier"; then
-    $notifier \
-      -group "homebrew-outdated-pkgs" \
-      -title "Newer formula available" \
-      -message "$pkgs" \
-      -sound Glass \
-      -sender com.apple.Terminal > /dev/null 2>&1
-  fi
+pkg_menuitems=$(echo "$pkgs" | $parallel -k get_pkg_menuitem)
+menuitems='
+{
+  "click": "'$brew' upgrade",
+  "text": "Upgrade all",
+  "refresh": true
+}, {
+  "text": "-"
+},'$pkg_menuitems
+
+echo '
+{
+  "imagesymbol": "circle.fill",
+  "text": "",
+  "menus": ['$menuitems']
+}'
+
+if test -x "$notifier"; then
+  $notifier \
+    -group "homebrew-outdated-pkgs" \
+    -title "Newer formula available" \
+    -message "$pkgs" \
+    -sound Glass \
+    -sender com.apple.Terminal > /dev/null 2>&1
 fi
 
